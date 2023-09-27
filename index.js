95ab63422e039ef1f20f51a0dc2a1d7ba5af0ece
webdev-ashishk <webdev.ashishk@gmail.com>
2023-09-27 10:15:24 +0530

    Lec-14 completed

--- index.js
+++ index.js
@@ -1,12 +1,58 @@
 "use strict";
-// Lec-7
-//assignment Question-1
-let message = "hello, Typescript!!";
-let id = 11111111;
-console.log(message);
-console.log("id is :" + id);
-//Quesiton-2
-function calculateArea(l, w) {
-    return l * w;
-}
-console.log("calculatedArea: " + calculateArea(100, 20000));
+const student1 = {
+    name: "ashishk",
+    class: "b.tech",
+    branch: "cs",
+    rollNo: 1234,
+    location: "sonebhadra",
+    address: {
+        village: "Muirpur",
+        pincode: 11111111,
+    },
+};
+const student2 = {
+    name: "nitin",
+    class: "b.tech",
+    branch: "cs",
+    rollNo: 12340,
+    location: "sonebhadra",
+    address: {
+        village: "Muirpur",
+        pincode: 22222222,
+    },
+};
+const student3 = {
+    name: "anju",
+    class: "b.tech",
+    branch: "cs",
+    rollNo: 12343,
+    location: "sonebhadra",
+    address: {
+        village: "Muirpur",
+        pincode: 3333333,
+    },
+};
+const student4 = {
+    name: "ashishk",
+    class: "b.tech",
+    branch: "cs",
+    rollNo: 12344,
+    location: "sonebhadra",
+    address: {
+        village: "Muirpur",
+        pincode: 4444444,
+    },
+};
+const student5 = {
+    name: "ashishk",
+    class: "b.tech",
+    branch: "cs",
+    rollNo: 12345,
+    location: "sonebhadra",
+    address: {
+        village: "Muirpur",
+        pincode: 5555555,
+    },
+};
+const p = student1.address.pincode;
+console.log(p);
--- index.ts
+++ index.ts
@@ -1,13 +1,66 @@
-// Lec-7
-//assignment Question-1
-let message = "hello, Typescript!!";
-let id = 11111111;
-console.log(message);
-console.log("id is :" + id);
-//Quesiton-2
+type Student = {
+  name: string;
+  class: string;
+  branch: string;
+  rollNo: number;
+  location: string;
+  address: { village: string; pincode: number };
+};
 
-function calculateArea(l: number, w: number) {
-  return l * w;
-}
-
-console.log("calculatedArea: " + calculateArea(100, 20000));
+const student1: Student = {
+  name: "ashishk",
+  class: "b.tech",
+  branch: "cs",
+  rollNo: 1234,
+  location: "sonebhadra",
+  address: {
+    village: "Muirpur",
+    pincode: 11111111,
+  },
+};
+const student2: Student = {
+  name: "nitin",
+  class: "b.tech",
+  branch: "cs",
+  rollNo: 12340,
+  location: "sonebhadra",
+  address: {
+    village: "Muirpur",
+    pincode: 22222222,
+  },
+};
+const student3: Student = {
+  name: "anju",
+  class: "b.tech",
+  branch: "cs",
+  rollNo: 12343,
+  location: "sonebhadra",
+  address: {
+    village: "Muirpur",
+    pincode: 3333333,
+  },
+};
+const student4: Student = {
+  name: "ashishk",
+  class: "b.tech",
+  branch: "cs",
+  rollNo: 12344,
+  location: "sonebhadra",
+  address: {
+    village: "Muirpur",
+    pincode: 4444444,
+  },
+};
+const student5: Student = {
+  name: "ashishk",
+  class: "b.tech",
+  branch: "cs",
+  rollNo: 12345,
+  location: "sonebhadra",
+  address: {
+    village: "Muirpur",
+    pincode: 5555555,
+  },
+};
+const p = student1.address.pincode;
+console.log(p);
