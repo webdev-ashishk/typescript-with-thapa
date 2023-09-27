92970da8c83665e9e2aa5c3ede782aff91f4377f
webdev-ashishk <webdev.ashishk@gmail.com>
2023-09-27 15:12:29 +0530

    Lec-13 assignment is completed

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 "use strict";
-var employee = ["anjuK", 10];
-employee.push("ashishk", 20);
-console.log("current-emp length " + employee.length);
-console.log(employee);
-// employee.push(true, "120");
-employee.pop();
-console.log(employee);
-let produc1 = ["samsung", 10000, 10];
-let produc2 = ["xomi", 9000, 90];
-function displayProductInfo(produc1) {
-    const [name, price, stock] = produc1;
-    console.log(`name of the Product is ${name} , price is ${price} and stock is ${stock}`);
-}
-displayProductInfo(produc1);
-displayProductInfo(produc2);
+const productObject = {
+    name: "HP",
+    price: 50000,
+    quantity: 10,
+};
+console.log(productObject.name);
+console.log(productObject.price);
+console.log(productObject.quantity);
--- index.ts
+++ index.ts
@@ -1,22 +1,14 @@
-var employee: [string, number] = ["anjuK", 10];
-employee.push("ashishk", 20);
-console.log("current-emp length " + employee.length);
-console.log(employee);
-// employee.push(true, "120");
-employee.pop();
-console.log(employee);
+type Product = {
+  name: string;
+  price: number;
+  quantity: number;
+};
+const productObject: Product = {
+  name: "HP",
+  price: 50000,
+  quantity: 10,
+};
 
-// assignment -one
-type ProductInfo = [string, number, number];
-
-let produc1: ProductInfo = ["samsung", 10000, 10];
-let produc2: ProductInfo = ["xomi", 9000, 90];
-
-function displayProductInfo(produc1: ProductInfo) {
-  const [name, price, stock] = produc1;
-  console.log(
-    `name of the Product is ${name} , price is ${price} and stock is ${stock}`
-  );
-}
-displayProductInfo(produc1);
-displayProductInfo(produc2);
+console.log(productObject.name);
+console.log(productObject.price);
+console.log(productObject.quantity);
