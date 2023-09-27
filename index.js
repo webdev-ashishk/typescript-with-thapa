29661788be8313efb2441329e4959309836533b0
webdev-ashishk <webdev.ashishk@gmail.com>
2023-09-27 14:15:42 +0530

    Lec-17 first assignment completed

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 "use strict";
-// Lec-7
-//assignment Question-1
-let message = "hello, Typescript!!";
-let id = 11111111;
-console.log(message);
-console.log("id is :" + id);
-//Quesiton-2
-function calculateArea(l, w) {
-    return l * w;
+var employee = ["anjuK", 10];
+employee.push("ashishk", 20);
+console.log("current-emp length " + employee.length);
+console.log(employee);
+// employee.push(true, "120");
+employee.pop();
+console.log(employee);
+let produc1 = ["samsung", 10000, 10];
+let produc2 = ["xomi", 9000, 90];
+function displayProductInfo(produc1) {
+    const [name, price, stock] = produc1;
+    console.log(`name of the Product is ${name} , price is ${price} and stock is ${stock}`);
 }
-console.log("calculatedArea: " + calculateArea(100, 20000));
+displayProductInfo(produc1);
+displayProductInfo(produc2);
--- index.ts
+++ index.ts
@@ -1,13 +1,22 @@
-// Lec-7
-//assignment Question-1
-let message = "hello, Typescript!!";
-let id = 11111111;
-console.log(message);
-console.log("id is :" + id);
-//Quesiton-2
+var employee: [string, number] = ["anjuK", 10];
+employee.push("ashishk", 20);
+console.log("current-emp length " + employee.length);
+console.log(employee);
+// employee.push(true, "120");
+employee.pop();
+console.log(employee);
 
-function calculateArea(l: number, w: number) {
-  return l * w;
+// assignment -one
+type ProductInfo = [string, number, number];
+
+let produc1: ProductInfo = ["samsung", 10000, 10];
+let produc2: ProductInfo = ["xomi", 9000, 90];
+
+function displayProductInfo(produc1: ProductInfo) {
+  const [name, price, stock] = produc1;
+  console.log(
+    `name of the Product is ${name} , price is ${price} and stock is ${stock}`
+  );
 }
-
-console.log("calculatedArea: " + calculateArea(100, 20000));
+displayProductInfo(produc1);
+displayProductInfo(produc2);
