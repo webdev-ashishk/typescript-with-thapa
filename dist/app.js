3d1fb3a60c7164ef012bc18d27bdf9edbcbdafc6
webdev-ashishk <webdev.ashishk@gmail.com>
2023-10-04 03:10:53 +0530

    calculator-mini function completed

--- dist/app.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-console.log("how are you two-three");
--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,19 @@
 "use strict";
-let a = 10;
-let b = 20;
-const result = a + b;
-console.log(result + 100);
+function calculator(a, b, c) {
+    if (c === "add") {
+        console.log(a + b);
+    }
+    else if (c === "mul") {
+        console.log(a * b);
+    }
+    else if (c === "div") {
+        console.log(a / b);
+    }
+    else if (c === "sub") {
+        console.log(a - b);
+    }
+}
+console.log(calculator(4, 5, "add"));
+console.log(calculator(4, 5, "mul"));
+console.log(calculator(4, 5, "div"));
+console.log(calculator(4, 5, "sub"));
