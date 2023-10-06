bc00dc953f4af9921d80e5d252021fe4fde40957
webdev-ashishk <webdev.ashishk@gmail.com>
2023-10-06 23:33:08 +0530

    props.tsx completed

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@ import Props from "@/components/Props";
 
 export default function Home() {
   return (
-    <div className="border-2 h-44 text-5xl font-bold m-10 p-10">
-      <Props heading="This is simple Heading" count={231208} />
+    <div className="border-2 w-[80vh] text-5xl font-bold m-10 p-10 rounded-lg border-purple-600">
+      <Props
+        heading="This is simple Heading"
+        count={231208}
+        title="props.tsx"
+      />
     </div>
   );
 }
--- components/Props.tsx
+++ components/Props.tsx
@@ -1,10 +1,12 @@
 type proptype = {
   heading: string;
   count: number;
+  title: string;
 };
-const Props = ({ heading, count }: proptype) => {
+const Props = ({ heading, count, title }: proptype) => {
   return (
     <div>
+      <p className="text-blue-500 text-center pb-3">{title}</p>
       <h1>{heading}</h1>
       <p>{count}</p>
     </div>
