e4b07ad10b8dc4496cdb54ce745e8eff375d9b4f
webdev-ashishk <webdev.ashishk@gmail.com>
2023-10-13 16:29:33 +0530

    challenge from other creater channels

---
+++ dist/challenge.js
@@ -0,0 +1,12 @@
+"use strict";
+// https://youtu.be/splSB6jDjIw
+// 2hr53min
+const lenghtofString = (str) => {
+    const length = str.length;
+    return length;
+};
+let store = lenghtofString("you are great");
+console.log(store);
+console.log(lenghtofString("123456789"));
+console.log(lenghtofString("hello world"));
+console.log(lenghtofString("you are welcome"));
